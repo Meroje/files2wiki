c2a4f247d0a69c7b0de8a4b2fdca4927abe4689f
Jérôme Foray <meroje.f@gmail.com>
2010-07-19 17:36:59 +0200

    Added function to view pages

---
+++ lib/view_page.php
@@ -0,0 +1,16 @@
+<?php
+function view_page($id) {
+    global $data;
+    $dir = "pages/$id";
+	
+	if (is_dir($dir)) {
+    $title = file_get_contents($dir.'/title.txt');
+    $content = file_get_contents($dir.'/content.txt');
+	
+    $data = array('Title'=>$title, 'Content'=>$content);
+	
+    return $data;
+	} else {
+		$data = NULL;
+	}
+}
